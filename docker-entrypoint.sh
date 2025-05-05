cad3ae444325b2474f1d695ec982ac6e0fd935a1
yshalsager <ysh-alsager@hotmail.com>
2025-05-05 23:17:38 +0300

    fix(docker): update entrypoint command to use app.asgi

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 API_PORT=${API_PORT:-5000}
-API_WORKERS=${API_WORKERS:-4}
+API_WORKERS=${API_WORKERS:-2}
 API_HOST=${API_HOST:-0.0.0.0}
 
-exec granian --interface asginl --host "$API_HOST" --port "$API_PORT" --workers "$API_WORKERS" app:app "$@" 
+exec granian --interface asginl --host "$API_HOST" --port "$API_PORT" --workers "$API_WORKERS" app:app.asgi "$@" 
