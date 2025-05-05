305a27c9daa471bed89c2bfedf61a12b3c88019f
yshalsager <ysh-alsager@hotmail.com>
2025-05-05 22:37:47 +0300

    fix(docker): use environment variable for API_HOST in docker-compose and entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-# Default values for environment variables if not set
 API_PORT=${API_PORT:-5000}
 API_WORKERS=${API_WORKERS:-4}
+API_HOST=${API_HOST:-0.0.0.0}
 
-# Execute the granian command directly
-exec granian --interface asginl --host 0.0.0.0 --port "$API_PORT" --workers "$API_WORKERS" app:app "$@" 
+exec granian --interface asginl --host "$API_HOST" --port "$API_PORT" --workers "$API_WORKERS" app:app "$@" 
